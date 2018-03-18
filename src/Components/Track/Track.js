24d467d36ad1e907a666c2912ee497673308ff75
Blaine Anderson <andersonblaine90@gmail.com>
2018-03-18 15:18:24 -0400

    Create Track Component

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+  renderAction() {
+    if (this.props.isRemoval) {
+      return '-';
+    } else {
+      return '+';
+    }
+  }
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>Track Name</h3>
+          <p>Artist | Album</p>
+        </div>
+        <a className="Track-action">{this.renderAction()}</a>
+      </div>
+    );
+  }
+}
+
+export default Track;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Track from '../Track/Track';
 import './TrackList.css';
 
 class TrackList extends React.Component {
