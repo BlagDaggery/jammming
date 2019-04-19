c6e89745c061c70c366ba402e5bc535bc098e98f
Blaine Anderson <andersonblaine90@gmail.com>
2019-04-19 11:25:33 -0400

    Updates

---
+++ precache-manifest.40e4ace53d8cad5b09fc48444cd3506c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "f5f9d50d5961f1446a57",
+    "url": "/jammming/static/js/runtime~main.85c7c486.js"
+  },
+  {
+    "revision": "b9409552ad0cebf1c450",
+    "url": "/jammming/static/js/main.441d9d60.chunk.js"
+  },
+  {
+    "revision": "fb43266046c2ede93742",
+    "url": "/jammming/static/js/2.c60eda27.chunk.js"
+  },
+  {
+    "revision": "b9409552ad0cebf1c450",
+    "url": "/jammming/static/css/main.249a31bb.chunk.css"
+  },
+  {
+    "revision": "897d501937c98922c4efe8fe3fccaaec",
+    "url": "/jammming/index.html"
+  }
+];
